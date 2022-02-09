a2143c4c37df40b7d1012024c7eaaee134bd1814
Youngnam <lyn00227@naver.com>
2022-02-09 23:04:38 +0900

    dto를 사용하는 컨트롤러 추가

--- src/main/java/hello/februarystudy/web/HelloController.java
+++ src/main/java/hello/februarystudy/web/HelloController.java
@@ -1,6 +1,8 @@
 package hello.februarystudy.web;
 
+import hello.februarystudy.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,12 @@ public class HelloController {
     @GetMapping("hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, // RequestParam 외부에서 API 로 넘긴 파라미터를 가져오는 어노테이션
+                                     @RequestParam("amount") int amount) {
+
+        return new HelloResponseDto(name, amount);
     }
 }
