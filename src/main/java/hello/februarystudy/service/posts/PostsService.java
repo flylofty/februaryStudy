be0b983b40807815c670e37cef0967526abcdf62
Youngnam <lyn00227@naver.com>
2022-02-11 16:07:00 +0900

    chapter 4

--- src/main/java/hello/februarystudy/service/posts/PostsService.java
+++ src/main/java/hello/februarystudy/service/posts/PostsService.java
@@ -2,12 +2,17 @@ package hello.februarystudy.service.posts;
 
 import hello.februarystudy.domain.posts.Posts;
 import hello.februarystudy.domain.posts.PostsRepository;
+import hello.februarystudy.web.dto.PostsListResponseDto;
 import hello.februarystudy.web.dto.PostsResponseDto;
 import hello.februarystudy.web.dto.PostsSaveRequestDto;
 import hello.februarystudy.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -23,7 +28,7 @@ public class PostsService {
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 업습니다. id=" + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
@@ -35,5 +40,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
---
+++ src/main/java/hello/februarystudy/web/dto/PostsListResponseDto.java
@@ -0,0 +1,22 @@
+package hello.februarystudy.web.dto;
+
+import hello.februarystudy.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
