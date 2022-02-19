377daa8706f37ec1e4d65d4395ab6ae79c305199
Youngnam <lyn00227@naver.com>
2022-02-20 01:30:56 +0900

    deploy t20

--- src/main/java/hello/februarystudy/web/PostsApiController.java
+++ src/main/java/hello/februarystudy/web/PostsApiController.java
@@ -1,11 +1,14 @@
 package hello.februarystudy.web;
 
 import hello.februarystudy.service.posts.PostsService;
+import hello.februarystudy.web.dto.PostsListResponseDto;
 import hello.februarystudy.web.dto.PostsResponseDto;
 import hello.februarystudy.web.dto.PostsSaveRequestDto;
 import hello.februarystudy.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,14 +26,19 @@ public class PostsApiController {
         return postsService.update(id, requestDto);
     }
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
     }
 
-    @DeleteMapping("/api/v1/posts/{id}")
-    public Long delete(@PathVariable Long id) {
-        postsService.delete(id);
-        return id;
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
     }
 }
