79b9de77352d08b851f4e9a306c4b9a8f334a858
Youngnam <lyn00227@naver.com>
2022-02-20 00:45:20 +0900

    deploy t19

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,11 +8,13 @@ REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=february-study
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/deloy/*.jar $REPOSITORY/
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
+
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
@@ -26,6 +28,7 @@ echo "> $JAR_NAME 실행"
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
+
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
